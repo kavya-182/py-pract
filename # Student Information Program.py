a4acd2810949c99a0e0ec76a3a728aa7bc9f81d3
kavya-182 <kavyakancherla971@gmail.com>
2026-07-28 22:39:15 -0700

    Add files via upload

---
+++ # Student Information Program.py
@@ -0,0 +1,16 @@
+# Student Information Program
+
+print("=== Student Information ===")
+
+# Input from the user
+student_name = input("Enter Student Name: ")
+roll_number = input("Enter Roll Number: ")
+course = input("Enter Course: ")
+age = int(input("Enter Age: "))
+
+# Display student details
+print("\n--- Student Details ---")
+print("Student Name :", student_name)
+print("Roll Number  :", roll_number)
+print("Course       :", course)
+print("Age          :", age)
