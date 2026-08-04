5ca01210c6bef4e749f15ad2af0103ec3c1c675e
kavya-182 <kavyakancherla971@gmail.com>
2026-08-04 10:26:30 +0530

    shape.py

--- shape.py
+++ shape.py
@@ -1,10 +1,8 @@
-# Parent Class
+
 class Shape:
     def area(self):
         print("Area of Shape")
 
-
-# Derived Class: Rectangle
 class Rectangle(Shape):
     def __init__(self, length, breadth):
         self.length = length
@@ -13,8 +11,6 @@ class Rectangle(Shape):
     def area(self):
         print("Area of Rectangle:", self.length * self.breadth)
 
-
-# Derived Class: Circle
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
@@ -22,8 +18,6 @@ class Circle(Shape):
     def area(self):
         print("Area of Circle:", 3.14 * self.radius * self.radius)
 
-
-# Main Program
 r = Rectangle(10, 5)
 c = Circle(7)
 
