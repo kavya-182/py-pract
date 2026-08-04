afbdb250c2cd32326d8f08fc73893043abd3ca66
kavya-182 <kavyakancherla971@gmail.com>
2026-08-04 07:16:24 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,42 @@
+# Parent Class
+class Employee:
+    def __init__(self, name, emp_id):
+        self.name = name
+        self.emp_id = emp_id
+
+    def display(self):
+        print("Employee Name:", self.name)
+        print("Employee ID:", self.emp_id)
+
+
+# Derived Class: Manager
+class Manager(Employee):
+    def __init__(self, name, emp_id, department):
+        super().__init__(name, emp_id)
+        self.department = department
+
+    def show_manager(self):
+        self.display()
+        print("Department:", self.department)
+
+
+# Derived Class: Developer
+class Developer(Employee):
+    def __init__(self, name, emp_id, language):
+        super().__init__(name, emp_id)
+        self.language = language
+
+    def show_developer(self):
+        self.display()
+        print("Programming Language:", self.language)
+
+
+# Main Program
+m = Manager("Ravi", 101, "HR")
+d = Developer("Kavyaa", 102, "Python")
+
+print("Manager Details")
+m.show_manager()
+
+print("\nDeveloper Details")
+d.show_developer()
